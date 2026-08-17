704ef0be40f95879ce50db5df7b2c3dc5d8cbf01
Chiru2170 <chaturvedichirag904@gmail.com>
2026-08-17 21:55:07 +0530

    Solved Sqrt(x

---
+++ Binary Search/Sqrt(x)_LC_69.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int lo = 1;
+        int hi = x;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if((mid) == x/mid){
+                return mid;
+            }
+            else if((mid) > x/mid){
+                hi = mid-1;
+            }
+            else{
+                lo = mid+1;
+            }
+        }
+        return hi;
+    }
+}
