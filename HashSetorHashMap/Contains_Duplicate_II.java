35efd14164b2c89be91fe9795720eda28f7114ba
Chiru2170 <chaturvedichirag904@gmail.com>
2026-08-28 09:34:34 +0530

    Solved Contains Duplicate II

---
+++ HashSetorHashMap/Contains_Duplicate_II.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int compliment = nums[i];
+            if(map.containsKey(compliment) && Math.abs(map.get(compliment)-i)<=k) return true;
+            else map.put(nums[i],i);
+        }
+        return false;
+    }
+}
