e125e0ad868c0edbf1a47729278c5d6548e98f5c
Chiru2170 <chaturvedichirag904@gmail.com>
2026-09-06 12:08:25 +0530

    Solved Maximum Substrings With Distinct Start

---
+++ Strings/Maximum_Substrings_With_Distinct_Start_LC_3760.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxDistinct(String s) {
+        // HashSet<Character> set = new HashSet<>();
+        // for(char ch : s.toCharArray()){
+        //     set.add(ch);
+        // }
+        // return set.size();
+
+        int[] freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        int count = 0;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>=1) count++;
+        }
+        return count;
+
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<s.length();i++){
+        //     if(sb.indexOf(String.valueOf(s.charAt(i))) == -1) sb.append(s.charAt(i));
+        // }
+        // return sb.length();
+    }
+}
